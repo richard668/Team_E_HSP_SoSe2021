f7cdab79a4c28690ea04d994e86ffa900ce23911
richard668 <richard.weber@student.jade-hs.de>
2021-03-24 18:14:51 +0100

    Update_1

--- Team_E_HSP_SoSe2021/Team_E_HSP_SoSe2021/Program.cs
+++ Team_E_HSP_SoSe2021/Team_E_HSP_SoSe2021/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
-namespace Team_E_HSP_SoSe2021
+/*
+ Hello World Programm
+ Version 1.0
+ letzte Aenderung 17.09.2019
+ Kai Mecke
+*/
+
+namespace Hello_World_Mecke
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // Hier beginnt das Hauptprogramm 
         {
-            Console.WriteLine("Hello World!");
-        }
+            // Ausgabe Benutzer 1
+            Console.WriteLine("Hello World von Jan");
+
+            // Ausgabe Benutzer 2
+            Console.WriteLine("Hello World von Marvin");
+
+            // Ausgabe Benutzer 3
+            Console.WriteLine("Hello World von Jan");
+
+            // Ausgabe Benutzer 4
+            Console.WriteLine("Hello World von Richard");
+
+            // Zeile die Konflikt triggern soll
+            Console.WriteLine("Hello");
+
+            // warte bis der User eine Taste drueckt
+            Console.ReadKey();
+
+        } // Hier endet das Hauptprogramm
     }
 }
