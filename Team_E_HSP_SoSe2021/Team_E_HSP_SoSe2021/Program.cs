0174351f670cb6c2e29b36ff62e89e1ad83961c5
richard668 <richard.weber@student.jade-hs.de>
2021-03-24 18:10:33 +0100

    Initial Commit

---
+++ Team_E_HSP_SoSe2021/Team_E_HSP_SoSe2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Team_E_HSP_SoSe2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
