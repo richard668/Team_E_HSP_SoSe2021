d6633bf42836ce87b4e2b6a3c35bdd215cf170a7
richard668 <richard.weber@student.jade-hs.de>
2021-04-02 21:07:58 +0200

    Schraubenberechnung Version 1.0
    Einfach mal laufen lassen und testen, die Berechnungen sollten noch hinzugefügt werden, sollte aber auch kein Problem sein. Weitere Eingaben und Ausgaben können auch noch hinzugefügt werden, aber bitte nicht zu viele.

--- Team_E_HSP_SoSe2021/Team_E_HSP_SoSe2021/Program.cs
+++ Team_E_HSP_SoSe2021/Team_E_HSP_SoSe2021/Program.cs
@@ -1,38 +1,196 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-/*
- Hello World Programm
- Version 1.1
- letzte Aenderung 25.03.2021
- Team_E_HSP
-*/
-
-namespace Hello_World_Team_E_HSP
+namespace ConsoleTest
 {
     class Program
     {
-        static void Main(string[] args) // Hier beginnt das Hauptprogramm 
+
+        static void Main(string[] args)
         {
-            // Ausgabe Benutzer 1
-            Console.WriteLine("Hello World von Jan1");
+            Console.Title = " Schraubenberechnung Team E";
 
-            // Ausgabe Benutzer 2
-            Console.WriteLine("Hello World von Marvin");
-
-            // Ausgabe Benutzer 3
-            Console.WriteLine("Hello World von Jan2");
-
-            // Ausgabe Benutzer 4
-            Console.WriteLine("Hello World von Richard");
-
-            // Zeile die Konflikt triggern soll
-
-            Console.WriteLine("Moin Team E");
-
-
-            // warte bis der User eine Taste drueckt
+            Console.WriteLine("\n\n\n Folgendes wird in diesem Programm ausgewertet..");
+            Console.WriteLine("\n\n Länge:");
+            Console.WriteLine(" Durchmessrer:");
+            Console.WriteLine(" Schraubenkopf:");
+            Console.WriteLine(" Material:");
+            Console.WriteLine(" Gewinde:");
+            Console.WriteLine("\n\n\n Zum Fortfahren ENTER drücken..");
             Console.ReadKey();
 
-        } // Hier endet das Hauptprogramm
+            string auswahl;
+            do
+            {
+                double laengenausgabe = Laenge(0);
+                Console.Clear();
+                Console.Clear();
+                string schraubenkopfausgabe = Schraubenkopf(0);
+                Console.Clear();
+                string materialausgabe = Material(0);
+                Console.Clear();
+                string gewindeausgabe = Gewinde(0);
+                Console.Clear();
+
+                Console.WriteLine("\n\n\n Folgende Eingabeparameter werden übermittelt..");
+                Console.WriteLine("\n\n Länge: " + laengenausgabe + " mm");
+                Console.WriteLine(" Durchmessrer: " + durchmesserausgabe + " mm");
+                Console.WriteLine(" Schraubenkopf: " + schraubenkopfausgabe);
+                Console.WriteLine(" Material: " + materialausgabe);
+                Console.WriteLine(" Gewinde: " + gewindeausgabe);
+
+                Console.WriteLine("\n\n Berechnete Größen: ");
+
+                //Hier kommen die Methoden für die Berechnungen hin..
+
+                Console.ReadKey();
+
+                Console.WriteLine("\n\n Neu berechnen? (j/n)");
+                auswahl = Console.ReadLine();
+            }
+            while (auswahl == "j");
+
+        }
+
+        static public double Laenge(int a)
+        {
+            Console.Clear();
+            double eingabeLaenge;
+
+            Console.WriteLine("\n Länge eingeben: \n");
+            eingabeLaenge = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("\n Gewählte Länge: " + eingabeLaenge + " mm");
+            Console.ReadKey();
+
+            return eingabeLaenge;
+
+        }
+
+        static public double Durchmesser(int b)
+        {
+            Console.Clear();
+            double eingabeDurchmesser;
+
+            Console.WriteLine("\n Durchmesser eingeben: \n");
+            eingabeDurchmesser = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("\n Gewählter Durchmesser: " + eingabeDurchmesser + " mm");
+            Console.ReadKey();
+
+            return eingabeDurchmesser;
+        }
+
+        static public string Schraubenkopf(int c)
+        {
+            Console.Clear();
+            int schraubenauswahl;
+            string schraubenart = "0";
+
+            Console.WriteLine("\n Schraubenkopf auswählen: ");
+            Console.WriteLine("\n <1> Sechskantschraube");
+            Console.WriteLine(" <2> Zylinderschraube mit Innensechskant");
+            Console.WriteLine(" <3> Senkkopf mit Innensechskant");
+
+            schraubenauswahl = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            switch (schraubenauswahl)
+            {
+                case 1:
+                    schraubenart = "Sechskantschraube";
+                    break;
+                case 2:
+                    schraubenart = "Zylinderschraube mit Innensechskant";
+                    break;
+                case 3:
+                    schraubenart = "Senkkopf mit Innensechskant";
+                    break;
+                default:
+                    schraubenart = "-/-";
+                    break;
+            }
+
+            Console.WriteLine("\n Gewählter Kopf: " + schraubenart);
+            Console.ReadKey();
+
+            return schraubenart;
+        }
+
+        static public string Material(int d)
+        {
+            int materialauswahl;
+            string materialart = "0";
+
+            Console.WriteLine("\n Material auswählen: ");
+            Console.WriteLine("\n <1> Material 1");
+            Console.WriteLine(" <2> Material 2");
+
+            materialauswahl = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            switch (materialauswahl)
+            {
+                case 1:
+                    materialart = "Material 1";
+                    break;
+                case 2:
+                    materialart = "Material 2";
+                    break;
+                default:
+                    materialart = "-/-";
+                    break;
+            }
+
+            Console.WriteLine("\n Gewähltes Material: " + materialart);
+            Console.ReadKey();
+
+            return materialart;
+        }
+
+        static public string Gewinde(int e)
+        {
+            int gewindeauswahl;
+            string gewindeart = "0";
+
+            Console.WriteLine("\n Gewinde auswählen: ");
+            Console.WriteLine("\n <1> Metrisches Regelgewinde");
+            Console.WriteLine(" <2> Metrisches Feingewinde");
+            Console.WriteLine(" <3> Trapezgewinde");
+            Console.WriteLine(" <4> Rohrgewinde");
+
+            gewindeauswahl = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            switch (gewindeauswahl)
+            {
+                case 1:
+                    gewindeart = "Metrisches Regelgewinde";
+                    break;
+                case 2:
+                    gewindeart = "Metrisches Feingewinde";
+                    break;
+                case 3:
+                    gewindeart = "Trapezgewinde";
+                    break;
+                case 4:
+                    gewindeart = "Rohrgewinde";
+                    break;
+                default:
+                    gewindeart = "-/-";
+                    break;
+            }
+
+            Console.WriteLine("\n Gewähltes Gewinde: " + gewindeart);
+            Console.ReadKey();
+
+            return gewindeart;
+        }
+
     }
+
 }
